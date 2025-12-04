e27efb8f6b28d296547f582db2eece417352ad23
edujoew <johnteedushiks247@gmail.com>
2025-12-04 14:40:38 +0300

    Implement Booking Submission View #5

--- resource_booking/booking/forms.py
+++ resource_booking/booking/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import BookingRequest
+from django.forms import DateTimeInput
+
 
 class BookingRequestForm(forms.ModelForm):
     """

8b252bcb9dd332e327c100fcbfe935b5fce28605
edujoew <johnteedushiks247@gmail.com>
2025-12-05 23:47:06 +0300

    admin pending request

--- resource_booking/booking/urls.py
+++ resource_booking/booking/urls.py
@@ -6,6 +6,7 @@ from .views import (
     RegisterView,
     my_bookings_dashboard,
 )
+from .views import admin_pending_requests, resource_list
 
 app_name = 'booking'
 
@@ -19,4 +20,7 @@ urlpatterns = [
     path('new/', BookingCreateView.as_view(), name='new_booking'),
 
     path('my_bookings_dashboard/', my_bookings_dashboard, name='my_bookings_dashboard'),
+
+    path('admin/pending/', admin_pending_requests, name='admin_pending_dashboard'),
+    path('resources/', resource_list, name='resource_list'),
 ]
